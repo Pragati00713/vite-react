9b221f75e7840b1a5b158e810e3934744730b27b
Pragati00713 <kushwahapragati713@gmail.com>
2023-09-12 22:08:52 +0530

    ty

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,22 +1,39 @@
 import React from 'react'
 import MyComponent from './MyComponent'
 import img from './Images/op1.jpeg'
+import imgs from './Images/op2.jpeg'
+import im from './Images/op3.jpeg'
+import imge from './Images/op5.jpeg'
+import r from './Images/realme1.jpeg'
+import rs from './Images/realme2.jpeg'
+import rss from './Images/realme3.jpeg'
+import rsss from './Images/realme3.jpeg'
+import rdmi from './Images/redmi1.jpeg'
 const Button = () => {
   return (
     <>
     
     <section className='d-flex' style={{flexWrap:"wrap",justifyContent:"center",alignItems:"center",border:"5px solid-red"}}>
 
-<MyComponent name="Oppo-a1" link="https://www.oppo.com/in/product/oppo-reno8-t.P.P1100122?variant=5011100203&gclid=Cj0KCQjwmICoBhDxARIsABXkXlICLL3fShJSFH7quR4c38oa4MMVHCbez8NGloobzwATYQnOe0rmFFIaAo3FEALw_wcB" img={img} price="29,000"></MyComponent>
-<MyComponent name="Oppo1" link="https://www.oppo.com/in/product/oppo-reno8-t.P.P1100122?variant=5011100203&gclid=Cj0KCQjwmICoBhDxARIsABXkXlICLL3fShJSFH7quR4c38oa4MMVHCbez8NGloobzwATYQnOe0rmFFIaAo3FEALw_wcB" img={img} price="₹39,999"></MyComponent>
-<MyComponent name="Oppo2" link="https://www.oppo.com/in/product/oppo-reno8-t.P.P1100122?variant=5011100203&gclid=Cj0KCQjwmICoBhDxARIsABXkXlICLL3fShJSFH7quR4c38oa4MMVHCbez8NGloobzwATYQnOe0rmFFIaAo3FEALw_wcB" img={img} price="₹19,999"></MyComponent>
-<MyComponent name="Oppo4" link="https://www.oppo.com/in/product/oppo-reno8-t.P.P1100122?variant=5011100203&gclid=Cj0KCQjwmICoBhDxARIsABXkXlICLL3fShJSFH7quR4c38oa4MMVHCbez8NGloobzwATYQnOe0rmFFIaAo3FEALw_wcB" img={img} price="₹28,999"></MyComponent>
-<MyComponent name="Realme" link="https://shop.gadgetsnow.com/smartphones/realme-c53-64-gb-champion-gold-6-gb-ram-/10021/p_G455562?gclid=Cj0KCQjwmICoBhDxARIsABXkXlLwcOzlImSSvv9jdLIedN58A5NFuTxm6bKYHN_o74fAwGjRFJcuDf4aAni6EALw_wcB" img={img} price="₹19,999"></MyComponent>
-<MyComponent name="Realme-A1" link="https://shop.gadgetsnow.com/smartphones/realme-c53-64-gb-champion-gold-6-gb-ram-/10021/p_G455562?gclid=Cj0KCQjwmICoBhDxARIsABXkXlLwcOzlImSSvv9jdLIedN58A5NFuTxm6bKYHN_o74fAwGjRFJcuDf4aAni6EALw_wcB" img={img} price="₹29,999"></MyComponent>
-<MyComponent name="Realme-A2" link="https://shop.gadgetsnow.com/smartphones/realme-c53-64-gb-champion-gold-6-gb-ram-/10021/p_G455562?gclid=Cj0KCQjwmICoBhDxARIsABXkXlLwcOzlImSSvv9jdLIedN58A5NFuTxm6bKYHN_o74fAwGjRFJcuDf4aAni6EALw_wcB" img={img} price="₹39,999"></MyComponent>
-<MyComponent name="Realme-A3" link="https://shop.gadgetsnow.com/smartphones/realme-c53-64-gb-champion-gold-6-gb-ram-/10021/p_G455562?gclid=Cj0KCQjwmICoBhDxARIsABXkXlLwcOzlImSSvv9jdLIedN58A5NFuTxm6bKYHN_o74fAwGjRFJcuDf4aAni6EALw_wcB" img={img} price="₹49,999"></MyComponent>
+<MyComponent name="Oppo-a1" link="https://www.oppo.com/in/product/oppo-reno8-t.P.P1100122?variant=5011100203&gclid=Cj0KCQjwmICoBhDxARIsABXkXlICLL
+3fShJSFH7quR4c38oa4MMVHCbez8NGloobzwATYQnOe0rmFFIaAo3FEALw_wcB" img={img} price="29,000"></MyComponent>
+<MyComponent name="Oppo1" link="https://www.oppo.com/in/product/oppo-ren
+o8-t.P.P1100122?variant=5011100203&gclid=Cj0KCQjwmICoBhDxARIsABXkXlICLL3fShJSFH7quR4c38oa4MM
+VHCbez8NGloobzwATYQnOe0rmFFIaAo3FEALw_wcB" img={imgs} price="₹39,999"></MyComponent>
+<MyComponent name="Oppo2" link="https://www.o
+ppo.com/in/product/oppo-reno8-t.P.P1100122?variant=5011100203&gclid=Cj0KCQjwmICoBhDxARIsABXkXlICLL3fShJSFH7quR4c38oa4MMVHCbez8NGloobzwATYQnOe0rmFFIaAo3FEALw_wcB" img={imge} price="₹19,999"></MyComponent>
+<MyComponent name="Oppo4" link="https://www.oppo.c
+om/in/product/oppo-reno8-t.P.P1100122?variant=50111
+00203&gclid=Cj0KCQjwmICoBhDxARIsABXkXlICLL3fShJSFH7quR4c38oa4MMVHCbez8NGloo
+bzwATYQnOe0rmFFIaAo3FEALw_wcB" img={im} price="₹28,999"></MyComponent>
+<MyComponent name="Realme" link="https://shop.gadgetsnow.com/smartphones/realme-c53-64-gb-champion-gold-6-gb-ram-/10021/p_G455562?gclid=Cj0KCQjwmICoBhDxARIsABXkXlLwcOzlImSSvv9jdLIedN58A5NFuTxm6bKYHN_o74fAwGjRFJcuDf4aAni6EALw_wcB" img={rsss} price="₹19,999"></MyComponent>
+<MyComponent name="Realme-A1" link="https://shop.gadgetsnow.com/smartphones/real
+me-c53-64-gb-champion-gold-6-gb-ram-/10021/p_G455562?gclid=Cj0KCQjwmICoBh
+DxARIsABXkXlLwcOzlImSSvv9jdLIedN58A5NFuTxm6bKYHN_o74fAwGjRFJcuDf4aAni6EALw_wcB" img={r} price="₹29,999"></MyComponent>
+<MyComponent name="Realme-A2" link="https://shop.gadgetsnow.com/smartphones/realme-c53-64-gb-champion-gold-6-gb-ram-/10021/p_G455562?gclid=Cj0KCQjwmICoBhDxARIsABXkXlLwcOzlImSSvv9jdLIedN58A5NFuTxm6bKYHN_o74fAwGjRFJcuDf4aAni6EALw_wcB" img={rs} price="₹39,999"></MyComponent>
+<MyComponent name="Realme-A3" link="https://shop.gadgetsnow.com/smartphones/realme-c53-64-gb-champion-gold-6-gb-ram-/10021/p_G455562?gclid=Cj0KCQjwmICoBhDxARIsABXkXlLwcOzlImSSvv9jdLIedN58A5NFuTxm6bKYHN_o74fAwGjRFJcuDf4aAni6EALw_wcB" img={rss} price="₹49,999"></MyComponent>
 <MyComponent name="Redmi-a2" link="https://www.google.com/search?q=redmi+phone+images&oq=redmi&aqs=chrome.0.69i59j0i67i131i433i650j69i64j0i67i433i650j46i67i433i613i650j69i60l3.2955j0j7&sourceid=chrome&ie=UTF-8#vhid=2kz5W7UAmMwP2M&vssid=l " img={img} price="₹25,999"></MyComponent>
-<MyComponent name="Redmi-a3" link="https://www.google.com/search?q=redmi+phone+images&oq=redmi&aqs=chrome.0.69i59j0i67i131i433i650j69i64j0i67i433i650j46i67i433i613i650j69i60l3.2955j0j7&sourceid=chrome&ie=UTF-8#vhid=2kz5W7UAmMwP2M&vssid=l" img={img} price="₹35,999"></MyComponent>
+<MyComponent name="Redmi-a3" link="https://www.google.com/search?q=redmi+phone+images&oq=redmi&aqs=chrome.0.69i59j0i67i131i433i650j69i64j0i67i433i650j46i67i433i613i650j69i60l3.2955j0j7&sourceid=chrome&ie=UTF-8#vhid=2kz5W7UAmMwP2M&vssid=l" img={rdmi} price="₹35,999"></MyComponent>
 <MyComponent name="Redmi-a4" link="https://www.google.com/search?q=redmi+phone+images&oq=redmi&aqs=chrome.0.69i59j0i67i131i433i650j69i64j0i67i433i650j46i67i433i613i650j69i60l3.2955j0j7&sourceid=chrome&ie=UTF-8#vhid=2kz5W7UAmMwP2M&vssid=l" img={img} price="₹45,999"></MyComponent>
 <MyComponent name="Redmi-a5" link="https://www.google.com/search?q=redmi+phone+images&oq=redmi&aqs=chrome.0.69i59j0i67i131i433i650j69i64j0i67i433i650j46i67i433i613i650j69i60l3.2955j0j7&sourceid=chrome&ie=UTF-8#vhid=2kz5W7UAmMwP2M&vssid=l" img={img} price="₹15,999"></MyComponent>
 <MyComponent name="Vivo-v14" link="https://www.google.com/search?q=vivo+mobiles+images&oq=vivo&aqs=chrome.0.69i59l2j69i57j69i64j0i67i433i650j46i67i199i433i465i650j0i67i650j46i67i433i613i650.2596j0j15&sourceid=chrome&ie=UTF-8#vhid=oeLcMS9GFLlHLM&vssid=l" img={img} price="₹45,999"></MyComponent>
